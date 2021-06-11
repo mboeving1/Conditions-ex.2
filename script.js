12ab1cbfeab995becb4641b022e815d7c9b4e75f
Matthew Boeving <mboeving1@gmail.com>
2021-06-10 22:13:30 -0400

    updated faulty code

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ var name2= "Mila Kunis";
 var name3= "Santa Claus";
 
 if(name1.length>name2.length && name1.length>name3.length){
-    console.log("Alex Rodriguez has the longest name.");
+    console.log(`${name1} has the longest name.`);
 }
 else if(name2.length>name1.length && name2.length>name3.length){
-    console.log("Mila Kunis has the longest name.")
+    console.log(`${name2} has the longest name.`)
 }
 else if(name3.length>name1.length && name3.length>name2.length){
-    console.log("Santa Claus has the longest name.");
+    console.log(`${name3} has the longest name.`);
 }
 else if(name1.length == name2.length && name1.length>name3.length){
-    console.log("Alex Rodriguez and Mila Kunis tie for the longest name.");
+    console.log(`${name1} and ${name2} tie for the longest name.`);
 }
 else if(name1.length == name3.length && name1.length>name2.length){
-    console.log("Alex Rodriguez and Santa Claus tie for the longest name.");
+    console.log(`${name1} and ${name3} tie for the longest name.`);
 }
 else if(name2.length == name3.length && name2.length>name1.length){
-    console.log("Mila Kunis and Santa Claus tie for the longest name.");
+    console.log(`${name2} and ${name3} tie for the longest name.`);
 }
 else{
     console.log("All three names are the same length");
